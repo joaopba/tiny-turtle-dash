1381ef9211f872642bdadeb7a390d61efd402f11
[dyad] <joaopedrorochabahiano@gmail.com>
2025-11-27 15:13:30 -0300

    [dyad] Corrigi o componente Layout para usar o Outlet do React Router, permitindo que as rotas aninhadas sejam renderizadas corretamente e solicitando ao usuário que verifique os logs novamente. - wrote 1 file(s)

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,16 +1,17 @@
 "use client";
 
 import React from "react";
+import { Outlet } from "react-router-dom"; // Importar Outlet
 import Sidebar from "./Sidebar";
 import { MadeWithDyad } from "./made-with-dyad";
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = () => { // Não precisamos mais da prop 'children' aqui
   return (
     <div className="flex min-h-screen bg-background text-foreground">
       <Sidebar />
       <main className="flex-1 flex flex-col">
         <div className="flex-1 p-6">
-          {children}
+          <Outlet /> {/* Renderizar o componente da rota aninhada aqui */}
         </div>
         <MadeWithDyad />
       </main>
